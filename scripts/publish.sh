6f887a5621aa615f4ae3e7ffce8da720e375f4b7
Martin Fernandez <fmartin91@gmail.com>
2019-01-11 02:55:00 -0300

    Fix permissions for gem credentials file

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,15 @@
+COMPILED_GEM_PATH=ddsl-$(scripts/version.sh).gem
+
+if [ ! -f $COMPILED_GEM_PATH ]; then
+  echo "Compiled gem not found at $COMPILED_GEM_PATH"
+  exit -1
+fi
+
+# Copy RUBYGEMS_TOKEN to the credentials file
 echo "---\n:rubygems_api_key: $RUBYGEMS_TOKEN" > ~/.gem/credentials
 
-gem push ddsl-$(scripts/version.sh).gem
+# Fix permissions
+chmod 0600 ~/.gem/credentials
+
+# Push current gem version
+gem push $COMPILED_GEM_PATH
